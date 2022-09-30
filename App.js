34c6030ee3454f418df2caf099257b2ac180189c
Peter Vang <vang_peter06@yahoo.com>
2022-09-30 11:56:47 -0700

    Add navigation and stack to App.js

--- App.js
+++ App.js
@@ -2,15 +2,25 @@ import { StatusBar } from 'expo-status-bar';
 import { Text, View, ImageBackground, TextInput, Button, KeyboardAvoidingView } from 'react-native';
 import LoginScreen from './screens/LoginScreen';
 import React from 'react';
-import InlineTextButton from './components/InlineTextButton';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   
 
   
   return (
-    <LoginScreen />
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{headerShown: false}} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    
       
       // <StatusBar style="auto" />
    
