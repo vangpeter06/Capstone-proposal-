e0fc6c36f5abf7143f2e0e5cb2ffb9265bfc13ba
Peter Vang <vang_peter06@yahoo.com>
2022-09-30 10:20:25 -0700

    Add inline button component and sign up button

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View, ImageBackground, TextInput } from 'react-native';
 import AppStyles from './styles/AppStyles';
+import React from 'react';
+import InlineTextButton from './components/InlineTextButton';
+
 
 export default function App() {
   
   const background = require("./assets/background.jpg");
+
+  let [email, setEmail] = React.useState("");
+  let [password, setPassword] = React.useState("");
   
   return (
     <ImageBackground style={AppStyles.container} source={background}>
@@ -13,11 +19,25 @@ export default function App() {
         <TextInput style={[AppStyles.textInput, AppStyles.lightTextInput, AppStyles.lightText,]}
         placeholder="Email"
         placeholderTextColor={"#BEBEBE"}
+        value={email}
+        onChangeText={setEmail}
         />
         <TextInput style={[AppStyles.textInput, AppStyles.lightTextInput, AppStyles.lightText,]}
         placeholder="Password"
         placeholderTextColor={"#BEBEBE"}
+        secureTextEntry={true}
+        value={password}
+        onChangeText={setPassword}
         />
+        <View style={[AppStyles.rowContainer, AppStyles.topMargin]}>
+          <Text style={AppStyles.lightText}>Don't have an account? </Text>
+          <InlineTextButton
+            text="Sign Up"
+            onPress={() => navigation.navigate("SignUp")}
+          />
+        </View>
+         
+      
 
         
       </View>
