d5fb32abfea55813293807f062c5b39fb156ab3b
Peter Vang <vang_peter06@yahoo.com>
2022-10-03 16:12:08 -0700

    WIP finding error

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import UserScreen from "./screens/UserScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator();    
 
 export default function App() {
   return (
