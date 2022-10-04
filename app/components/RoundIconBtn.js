0b459d8b4254da5d1a63499b70b2fbb740fd355a
Peter Vang <vang_peter06@yahoo.com>
2022-10-04 13:06:35 -0700

    add btn component

---
+++ app/components/RoundIconBtn.js
@@ -0,0 +1,30 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { AntDesign } from '@expo/vector-icons';
+import colors from '../misc/colors';
+
+const RoundIconBtn = ({antIconName, size, color, style, onPress}) => {
+  return (
+  <AntDesign
+  name= {antIconName}
+  size= {size || 24}
+  color={color || colors.LIGHT}
+  style={[styles.icon, { ...style }]}
+  onPress={onPress}
+  />
+    
+  )
+}
+
+export default RoundIconBtn
+
+const styles = StyleSheet.create({
+  icon:{
+    backgroundColor: colors.PRIMARY,
+    padding: 15,
+    borderRadius: 50,
+    elevation: 5,
+    shadowOffset: 5,
+  },
+})
+
