bb326e785b2810e61ca4d70295ae8e1b017e2a85
Peter Vang <vang_peter06@yahoo.com>
2022-10-04 13:29:24 -0700

    Add greeting time of the day to greet

--- app/screens/NoteScreen.js
+++ app/screens/NoteScreen.js
@@ -1,17 +1,28 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { StatusBar } from 'expo-status-bar'
 import colors from '../misc/colors'
 
 const NoteScreen = ({user}) => {
 
-  const [greet, setGreet] = useState('Evening');
+  const [greet, setGreet] = useState('');
+
+  const findGreet = () => {
+    const hrs = new Date().getHours();
+    if (hrs === 0 || hrs < 12) return setGreet('Morning');
+    if (hrs === 1 || hrs < 17) return setGreet('Afternoon');
+    setGreet('Evening');
+  }
+
+  useEffect (() => {
+    findGreet();
+  }, []);
 
   return (
     <>
       <StatusBar barStyle='dark-content' backgroundColor={colors.LIGHT} />
       <View style={styles.container}>
-        <Text>{`Good ${greet} ${user.name}`}</Text>
+        <Text style={styles.header}>{`Good ${greet} ${user.name}`}</Text>
       </View>
     </>
   )
@@ -20,7 +31,13 @@ const NoteScreen = ({user}) => {
 export default NoteScreen
 
 const styles = StyleSheet.create({
-  container:{
-
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  header: {
+    FontSize: 25,
+    fontWeight: 'bold',
   },
 })
